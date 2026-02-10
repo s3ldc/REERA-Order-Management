f0566f693f9d6ac71d7859c874befc225c112d77
Biriy <biriyasunil25@gmail.com>
2026-02-10 22:17:14 +0530

    Removed the comment

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,6 +2,6 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 
-console.log("PB URL:", import.meta.env.VITE_PB_URL);
+// console.log("PB URL:", import.meta.env.VITE_PB_URL);
 
 export default pb;
