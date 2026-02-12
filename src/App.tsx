bae6f82a4bf7e998e6990f46846b65a8066b5de2
Biriy <biriyasunil25@gmail.com>
2026-02-12 21:52:27 +0530

    refactor: replace test JSON output with mapped user list UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { useQuery, useMutation } from "convex/react";
+import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
 export default function App() {
   const users = useQuery(api.users.getUsers);
-  const createUser = useMutation(api.users.createUser);
 
   if (users === undefined) return <div>Loading...</div>;
 
   return (
     <div style={{ padding: 20 }}>
       <h1>Users</h1>
-
-      <button
-        onClick={() =>
-          createUser({
-            name: "Sunil",
-            email: "sunil@test.com",
-            role: "Admin",
-          })
-        }
-      >
-        Add User
-      </button>
-
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      {users.map((user) => (
+        <div key={user._id}>
+          {user.name} - {user.role}
+        </div>
+      ))}
     </div>
   );
 }
