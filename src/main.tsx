2752e4c278f65b14f698a83d26e25d2eb2ba880e
Biriy <biriyasunil25@gmail.com>
2026-02-12 21:42:27 +0530

    feat: integrate ConvexProvider and initialize ConvexReactClient in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-)
+const convex = new ConvexReactClient(
+  import.meta.env.VITE_CONVEX_URL || import.meta.env.NEXT_PUBLIC_CONVEX_URL
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ConvexProvider client={convex}>
+      <App />
+    </ConvexProvider>
+  </React.StrictMode>
+);
