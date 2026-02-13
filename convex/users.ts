1f0eac57ddcc2d798124f28601725094a9c929de
Biriy <biriyasunil25@gmail.com>
2026-02-13 12:04:15 +0530

    feat(auth): add getUserByEmail query for secure login action

--- convex/users.ts
+++ convex/users.ts
@@ -1,6 +1,19 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import bcrypt from "bcryptjs";
+
+
+export const getUserByEmail = query({
+  args: {
+    email: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("email"), args.email))
+      .first();
+  },
+});
 
 export const createUser = mutation({
   args: {
